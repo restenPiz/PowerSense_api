b4271ec43df787c621bb5c44b94618f859d3285b
Mauro Peniel <99766054+restenPiz@users.noreply.github.com>
2026-02-11 10:49:06 +0200

    Doing some updates in the api routes file

--- powersense-api/routes/api.php
+++ powersense-api/routes/api.php
@@ -2,7 +2,29 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\RecargaController;
+use App\Http\Controllers\Api\ContadorController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+
+    // Auth
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/me', [AuthController::class, 'me']);
+
+    // Dashboard
+    Route::get('/dashboard', [ContadorController::class, 'dashboard']);
+    Route::get('/consumo/semanal', [ContadorController::class, 'consumoSemanal']);
+
+    // Recargas
+    Route::post('/recarga', [RecargaController::class, 'store']);
+    Route::get('/recargas', [RecargaController::class, 'index']);
+    Route::get('/recarga/{id}', [RecargaController::class, 'show']);
+});
