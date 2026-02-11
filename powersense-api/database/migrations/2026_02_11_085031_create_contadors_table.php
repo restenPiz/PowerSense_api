b99b75b26c9ab4e0cae50d0d7add4c082bc109ad
Mauro Peniel <99766054+restenPiz@users.noreply.github.com>
2026-02-11 23:35:42 +0200

    Doing some updates in the application

--- powersense-api/database/migrations/2026_02_11_085031_create_contadors_table.php
+++ powersense-api/database/migrations/2026_02_11_085031_create_contadors_table.php
@@ -10,7 +10,7 @@ return new class extends Migration {
      */
     public function up(): void
     {
-        Schema::create('contadors', function (Blueprint $table) {
+        Schema::create('contadores', function (Blueprint $table) {
             $table->id();
             $table->string('numero_contador', 50)->unique();
             $table->string('nome_proprietario', 255);
@@ -29,6 +29,6 @@ return new class extends Migration {
      */
     public function down(): void
     {
-        Schema::dropIfExists('contadors');
+        Schema::dropIfExists('contadores');
     }
 };
