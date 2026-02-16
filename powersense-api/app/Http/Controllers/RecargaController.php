6cdb86954f5dde78b2659d84caa931b01f3b1db8
Mauro Peniel <99766054+restenPiz@users.noreply.github.com>
2026-02-16 12:39:12 +0200

    Doing some updates in the navbar

--- powersense-api/app/Http/Controllers/RecargaController.php
+++ powersense-api/app/Http/Controllers/RecargaController.php
@@ -9,8 +9,8 @@ class RecargaController extends Controller
 {
     public function index()
     {
-        $recargas = Recarga::orderBy('data_recarga', 'desc')->paginate(20);
-
-        return view('recargas.index', compact('recargas'));
+        return \Inertia\Inertia::render('Pages/Recarga', [
+            'recargas' => Recarga::all()
+        ]);
     }
 }
