00cf3ceea329f97f1d0797de5948b52f5dc13a87
Mauro Peniel <99766054+restenPiz@users.noreply.github.com>
2026-02-16 12:09:47 +0200

    Doing some updates in the application

--- powersense-api/app/Http/Controllers/RecargaController.php
+++ powersense-api/app/Http/Controllers/RecargaController.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Recarga;
 
 class RecargaController extends Controller
 {
-    //
+    public function index()
+    {
+        $recargas = Recarga::orderBy('data_recarga', 'desc')->paginate(20);
+
+        return view('recargas.index', compact('recargas'));
+    }
 }
