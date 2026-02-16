0c4508fffdc7f199f297ae920b1db52ca5ad88e6
Mauro Peniel <99766054+restenPiz@users.noreply.github.com>
2026-02-16 12:12:07 +0200

    Update the routes files

--- powersense-api/routes/web.php
+++ powersense-api/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\RecargaController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -14,9 +15,14 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+
+    //*Profile Routes
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    //*Recarga Routes
+    Route::get('/recargas', [RecargaController::class, 'index'])->name('recargas.index');
 });
 
 require __DIR__ . '/auth.php';
